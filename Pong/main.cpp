dcf42b99fc37eb67cf1a7af5c54c9e5369a67724
SpookyStreet <tom_street@hotmail.co.uk>
2024-04-18 15:52:45 +0100

    Drawing intial objects

--- Pong/main.cpp
+++ Pong/main.cpp
@@ -3,12 +3,32 @@
 
 int main()
 {
-	InitWindow(800, 600, "Pong");
+	const int screen_width = 1280; // variables for window size 
+	const int screen_height = 800;
+	
+	const int ball_radius = 10;
+	const int paddle_width = 20;
+	const int paddle_height = 100;
+	const int paddle_offset = 10;
 
-	while (!WindowShouldClose())
+
+	InitWindow(screen_width, screen_height, "Pong");
+	SetTargetFPS(60); // game to run at 60 FPS
+
+	// game loop
+	while (WindowShouldClose() == false) 
 	{
-		BeginDrawing();
-			ClearBackground(BLACK);
+		BeginDrawing();	
+		
+		//Drawing game objects
+		DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
+		DrawCircle(screen_width/2, screen_height/2, ball_radius, WHITE);
+		DrawRectangle(paddle_offset, screen_height / 2 - paddle_height / 2, paddle_width, paddle_height, WHITE);
+		DrawRectangle(screen_width - paddle_width - paddle_offset, screen_height / 2 - paddle_height / 2, paddle_width, paddle_height, WHITE);
+		
+
+
+		//DrawFPS(10, 10); //displays FPS 
 		EndDrawing();
 	}
 
