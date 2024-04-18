e744ec02bf4b8cd4c39a24012a0bfa4258dabef7
SpookyStreet <tom_street@hotmail.co.uk>
2024-04-18 14:41:16 +0100

    Add project files.

---
+++ Pong/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <raylib.h>
+
+int main()
+{
+	InitWindow(800, 600, "Pong");
+
+	while (!WindowShouldClose())
+	{
+		BeginDrawing();
+			ClearBackground(BLACK);
+		EndDrawing();
+	}
+
+	CloseWindow();
+}
